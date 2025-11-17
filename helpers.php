e089ce5c88848ca37ae11be5a1107c82ab1843ba
Tokotimas <tomas.katinas1996@gmail.com>
2025-11-17 19:07:50 +0200

    Added helped function to load partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,41 @@ function basePath($path = ''): string
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ *
+ * @param string $name
+ * @return void
+ */
+function loadView($name): void
+{
+    $viewPath =  basePath(path: "views/{$name}.view.php");
+
+    if(file_exists(filename:$viewPath)){
+        require $viewPath;
+    }
+
+    else{
+        echo "View '{$name}' not found!";
+    }
+}
+
+/**
+ * Load a partial
+ *
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name): void
+{
+    $partialPath =  basePath(path: "views/partials/{$name}.php");
+
+    if(file_exists(filename:$partialPath)){
+        require $partialPath;
+    }
+
+    else{
+        echo "View '{$name}' not found!";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php declare(strict_types=1);
 
 require '../helpers.php';
-require basePath('views/home.view.php');
+loadView(name: 'home');
